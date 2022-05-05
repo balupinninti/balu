826e4bbcd3b2ba9d830e1d778dafa68d96f8b964
balupinninti <104815292+balupinninti@users.noreply.github.com>
2022-05-05 10:48:21 +0530

    Update FileLib.java

--- Raptor/src/main/java/com/Daimi/GenericLib/FileLib.java
+++ Raptor/src/main/java/com/Daimi/GenericLib/FileLib.java
@@ -10,6 +10,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class FileLib {
 //pushing from eclipse to github
+//pulling from github to eclipse	
 	public String readPropertyData(String path, String key) throws Throwable
 	{
 		FileInputStream fis = new FileInputStream(path);
